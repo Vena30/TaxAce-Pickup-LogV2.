cfc2d6633ef5fa70205edc2ff3f295146daa9b74
Research & Evaluation Lead <ravena@taxacebsi.com>
2026-09-10 03:17:25 -0700

    Rename 0005_late_speed_demon.sql to drizzle/0005_late_speed_demon.sql

--- 0005_late_speed_demon.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `tax_year_records` MODIFY COLUMN `status` enum('In Vault','Contacted','Scheduled','Prepped for Pickup','Picked Up','Prep to Shred','Shredded','Hold') NOT NULL DEFAULT 'In Vault';
